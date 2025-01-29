bb2dc104bebbc57a85fb217c2bf6a0e098822ade
Carlos V <carlosv.provoos@gmail.com>
2025-01-29 16:34:50 -0500

    add dist

---
+++ dist/index.d.ts
@@ -0,0 +1,9 @@
+declare const twitterPlugin: {
+    name: string;
+    description: string;
+    actions: Action[];
+    evaluators: never[];
+    providers: never[];
+};
+
+export { twitterPlugin as default, twitterPlugin };
